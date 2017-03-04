bf7894863bdca9846455e14c21d1816f0b83deb6
Shirfeng Chang <shirfeng.chang@gmail.com>
2017-03-04 02:40:08 -0800

    initial chef DK setup

---
+++ README.md
@@ -0,0 +1,4 @@
+# vagrantlabchef
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/vagrantlabchef/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: vagrantlabchef
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
